186570051858e2a5bd731d65b6e660f13f4a5304
Pratama Dimas <pratamadimas.pw@gmail.com>
2025-04-15 15:58:58 +0700

    Re-Init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <main>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Welcome to my 3D Portfolio!
+        </h1>
+      </div>
+    </main>
+  );
+}
