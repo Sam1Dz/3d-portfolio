2fc0e9efaf386733684bd63dbf62b1c5bec8a426
Pratama Dimas <pratamadimas.pw@gmail.com>
2025-04-17 18:52:47 +0700

    Add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 /* SECTIONS */
 import Hero from './sections/Hero';
 import Showcase from './sections/Showcase';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <React.Fragment>
+      <Navbar />
       <Hero />
       <Showcase />
     </React.Fragment>
